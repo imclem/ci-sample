4b1085473bb22588cf6768f8b8c2a283f3e90c70
Clement Agarini <716892+imclem@users.noreply.github.com>
2019-10-23 14:37:05 +0200

    Initial

---
+++ src/main/java/fr/iut/CiSample.java
@@ -0,0 +1,10 @@
+package fr.iut;
+
+import org.apache.commons.text.WordUtils;
+
+public class CiSample {
+
+    public static void main(String[] args) {
+        System.out.println(WordUtils.capitalize("hello world"));
+    }
+}
---
+++ src/test/java/fr/iut/TestCiSample.java
@@ -0,0 +1,13 @@
+package fr.iut;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestCiSample {
+
+    @Test
+    public void testTrueIsTrue() {
+        assertTrue(true);
+    }
+}
